2ec744a86d445ff62413c3c9e99cdf0df7e4d5ce
unknown <pghossoub@hotmail.fr>
2020-02-11 18:26:13 +0100

    first commit. Some Anims & move player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator _anim;
+    private Rigidbody2D _rb;
+    private Transform _tr;
+
+    public float m_speed;
+    public float m_maxVelocityX;
+
+
+    void Start()
+    {
+        _tr = gameObject.transform;
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponentInChildren<Animator>();
+
+    }
+
+    void Update()
+    {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        _rb.velocity = new Vector2(direction.x * m_speed * Time.deltaTime, _rb.velocity.y);
+
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+
+           _tr.localScale = Vector3.one;
+           //_anim.SetBool(Animator.StringToHash("IsWalking"), true);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+
+            _tr.localScale = new Vector3(-1, 1, 1);
+            //_anim.SetBool(Animator.StringToHash("IsWalking"), true);
+        }
+
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            //_anim.SetBool(Animator.StringToHash("IsWalking"), false);
+            //_rb.velocity = new Vector2(0, _rb.velocity.y);
+        }
+
+        /*
+        if(_rb.velocity.x > m_maxVelocityX)
+        {
+            _rb.velocity = new Vector2(m_maxVelocityX, _rb.velocity.y);
+        }
+
+        if (_rb.velocity.x < - m_maxVelocityX)
+        {
+            _rb.velocity = new Vector2(- m_maxVelocityX, _rb.velocity.y);
+        }
+        */
+        _anim.SetFloat(Animator.StringToHash("VelocityX"), Mathf.Abs(_rb.velocity.x) / m_maxVelocityX);
+
+
+
+
+    }
+
+}
